6a70a9a3817823355bafa155fe6869f7b0a2ed1a
Levaaa123t <143887693+Levaaa123t@users.noreply.github.com>
2025-02-24 18:26:14 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,20 +4,52 @@ from config import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
 #Объект бот из класса
+@bot.message_handler(commands=['help'])
+def handle_start(message):
+    bot.send_message(message.chat.id, '''Привет! Я бот, вот команды которые ты можешь использовать при роботе со мной
+                    /start - знакомство с ботом и что он умеет
+                    /best_film - выбор фильма по твоим критериям
+                    ''')
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.send_message(message.chat.id, "Привет! Я бот, который ")
+    bot.send_message(message.chat.id, "Привет! Я бот, который может подбирать фильмы по критериям, напиши /help для большего количества функций")
 
 @bot.message_handler(commands=['best_film'])
 def get_genre(message):
-    genre_question = bot.send_message(message.chat.id, 'Напиши :')
-    bot.register_next_step_handler(genre_question, get_year)  
+    genre_question = bot.send_message(message.chat.id, '''Напиши жанр который ты хочешь из этих:
+                                                        1.Animation
+                                                        2.Adventure
+                                                        3.Comedy
+                                                        4.Action
+                                                        5.Thriller
+                                                        6.Drama
+                                                        7.Romance
+                                                        8.Biography
+                                                        9.History
+                                                        10.Crime
+                                                        11.Mystery
+                                                        12.Family
+                                                        13.War
+                                                        14.Fantasy
+                                                        15.Music
+                                                        16.Horror
+                                                        17.Sport
+                                                        18.Sci-Fi
+                                                        19.Documentary
+                                                        20.Western
+                                                        21.Musical
+                                                        22.Film-Noir
+                                                        23.Short
+                                                        24.News''')
+    bot.register_next_step_handler(genre_question, get_vote)  
 
-def get_year(message): 
-    year = message.text
-    style_question = bot.send_message(message.chat.id, '')
-    bot.register_next_step_handler(style_question, year) 
+def get_vote(message): 
+    vote = message.text
+    style_question = bot.send_message(message.chat.id, 'С какой оценки начать поиск от 1 до 10?')
+    bot.register_next_step_handler(style_question, id) 
+
+    
 
 
 
